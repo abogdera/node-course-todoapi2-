dce69c1bfe2e808fc637b20e6cc0fed1c2623f79
abougdera <abougder@yahoo.com>
2018-11-10 21:05:20 -0700

    njbjhbjh

---
+++ server/server.js
@@ -0,0 +1,55 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+
+
+
+
+var {mongoos}=require('./db/mongoose.js')
+var {Todo}=require('./models/todo.js')
+var {user}=require('./models/user.js')
+
+var app=express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+var todo =new Todo({
+  text:req.body.text
+})
+
+todo.save().then((doc)=>{
+  res.send(doc)
+},(e)=>{
+  res.status(400).send(e)
+})
+})
+app.listen(3000,()=>{
+console.log('started on port 3000')
+
+})
+/* var newTodo = new Todo({
+text:'cook dinner'
+}
+)
+
+newTodo.save().then((doc)=>{
+  console.log('saved Todo',doc)
+
+},(e)=>{
+console.log('unable to save Todo')
+
+}) */
+/* var otherTodo= new Todo({
+  text:'start with fun',
+  //completed:true,
+ // completedAt:123
+}) 
+
+otherTodo.save().then(()=>{
+  console.log('saved Todo',JSON.stringify(doc,undefined,2));
+},(e)=>{
+
+  console.log('unable to save Todo')
+ 
+});*/
+
+
